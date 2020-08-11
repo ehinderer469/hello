7489a45497feee18bbe7a0e1b2ff7c5fd2427f06
Eric Hinderer <ehinderer2@huskers.unl.edu>
2020-08-11 00:02:02 -0500

    Got rid of all of my test cases

--- hello.c
+++ hello.c
@@ -4,7 +4,7 @@
  * Date: 2020/08/10
  *
  * A simple hello world program in C
- * Adding this line as a test 
+ *
  */
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,9 +13,6 @@ int main(int argc, char **argv) {
 
   printf("Eric Hinderer\n");
   printf("I am majoring in computer science\n");
-
-  int a = 5;
-  printf("%d\n", a);
 
   return 0;
 }
