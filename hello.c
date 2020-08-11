b24f0a2f2282df96aa692c4775aae5d25d5f7e1e
Eric Hinderer <ehinderer2@huskers.unl.edu>
2020-08-10 23:40:20 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Eruc Hinderer
+ *
+ * Date: 2020/08/10
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Eric Hinderer\n");
+
+  return 0;
+}
