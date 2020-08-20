f2b3c705afd8ad82bf4a66b95c74b12c5a48b227
Eric Hinderer <ehinderer2@huskers.unl.edu>
2020-08-19 21:52:19 -0500

    accident

--- hello.c
+++ hello.c
@@ -1,16 +1,16 @@
 /**
- * Author: Eric Hinderer
+ * Author: Eric Hinderer, Austin Lempke
  *
- * Date: 2020/08/10
+ * Date: 2020/08/19
  *
- * A simple program  that prints my name and major in C
+ * A simple program that prints my name and partner's name and major in C
  *
  */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Eric Hinderer\n");
+  printf("Eric Hinderer!\n");
   printf("I am majoring in computer science\n");
 
 
