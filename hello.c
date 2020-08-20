b0b5007690bab8fb62205d91ed17f99e5b7a3cc2
Eric Hinderer <ehinderer2@huskers.unl.edu>
2020-08-19 21:19:10 -0500

    Got rid of a print statement

--- hello.c
+++ hello.c
@@ -10,9 +10,9 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Austin Lempke\n");
   printf("Eric Hinderer\n");
   printf("I am majoring in computer science\n");
+
 
   return 0;
 }
